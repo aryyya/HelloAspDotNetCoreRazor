aeee3bbbc131a81040d1809063b2e193a6f1ce1f
aryyya <arya@live.ca>
2018-05-02 15:16:04 -0700

    Add more fields to customer model.

--- HelloAspDotNetCoreRazor/Customer.cs
+++ HelloAspDotNetCoreRazor/Customer.cs
@@ -8,5 +8,16 @@ namespace HelloAspDotNetCoreRazor
 
         [Required, StringLength(64)]
         public string Name { get; set; }
+
+        [Required, StringLength(64)]
+        public string Email { get; set; }
+
+        [Required]
+        public int BirthYear { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {Name}, Email: {Email}, BirthYear: {BirthYear}";
+        }
     }
 }
