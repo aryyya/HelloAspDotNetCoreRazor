bf9998fdb8d0d7b06a9a5757dc3278937b8f4ec5
aryyya <arya@live.ca>
2018-05-01 18:59:32 -0700

    Use in memory database.

---
+++ HelloAspDotNetCoreRazor/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HelloAspDotNetCoreRazor
+{
+    internal class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options)
+            : base(options)
+        {
+            
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
